764d718efa3c871e6e017782dfa1bbe32ee6e6ed
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2022-05-05 20:07:53 -0300

    :lipstick: Altera fonte para Chakra Petch

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,12 @@ export default function Home() {
         <meta name="author" content="Strange Mike" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300&family=Quicksand:wght@500&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         />
       </Head>
